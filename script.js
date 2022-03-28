9487d280f1f7f381112a9fb8a9765090de85b011
tProtheroe <tomprothe@hotmail.co.uk>
2022-03-28 23:24:36 +0100

    Display clicked numbers and operators

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const display = document.querySelector('.display');
+const calculate = document.querySelector('.equals');
 
+let displayValue = '';
 
 function add(num1, num2){
     return num1 + num2;
@@ -19,3 +22,19 @@ function divide(num1, num2){
 function operate(operator, num1, num2){
     return operator(num1, num2);
 }
+
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+
+numbers.forEach(num => num.addEventListener('click', () => {
+    displayValue = num.textContent;
+    display.textContent += displayValue;
+}))   
+
+operators.forEach(op => op.addEventListener('click', () => {
+    displayValue = op.textContent;
+    display.textContent += displayValue;
+}))
+
+calculate.addEventListener('click', operate());
+
