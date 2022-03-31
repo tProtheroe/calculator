323a43c1cc390d08037be187fed76e81251d729a
tProtheroe <tomprothe@hotmail.co.uk>
2022-03-31 17:13:17 +0100

    Calculator can process two numbers, some styling changes

--- script.js
+++ script.js
@@ -1,40 +1,89 @@
-const display = document.querySelector('.display');
-const calculate = document.querySelector('.equals');
-
-let displayValue = '';
-
-function add(num1, num2){
-    return num1 + num2;
-}
-
-function subtract(num1, num2){
-    return num1 - num2;
-}
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function divide(num1, num2){
-    return num1 / num2;
-}
-
-function operate(operator, num1, num2){
-    return operator(num1, num2);
-}
-
+const currentDisplayValue = document.querySelector('.current-number');
+const clear = document.querySelector('.clear');
+const backspace = document.querySelector('.backspace');
+const equals = document.querySelector('.equals');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+const previousDisplayValue = document.querySelector('.previous-number');
 
-numbers.forEach(num => num.addEventListener('click', () => {
-    displayValue = num.textContent;
-    display.textContent += displayValue;
-}))   
+let currentInput = '';
+let previousInput = '';
+let operator = ''; 
+let result = 0;
 
-operators.forEach(op => op.addEventListener('click', () => {
-    displayValue = op.textContent;
-    display.textContent += displayValue;
+function calculate(){
+
+    previousInput = Number(previousInput);
+    currentInput = Number(currentInput);
+
+    switch (operator){
+        case '+':
+            previousInput += currentInput;
+            break;
+        case '-':
+            previousInput -= currentInput;
+            break;
+        case '*':
+            previousInput *= currentInput;
+            break;
+        case '/':
+            previousInput /= currentInput;
+            break;
+    }
+
+    roundNum();
+
+    previousDisplayValue.textContent = '';
+    currentDisplayValue.textContent = previousInput
+}
+
+function handleNum(num){
+    if(currentInput.length <= 14){
+        currentInput += num;
+        currentDisplayValue.textContent += num;
+    }
+}
+
+function handleOp(op){
+    operator = op;
+    previousInput = currentInput
+    currentInput = '';
+    previousDisplayValue.textContent = `${previousInput} ${op}`
+    currentDisplayValue.textContent = '';
+}
+
+function roundNum(){
+    previousInput = Math.round(previousInput * 1000) / 1000;
+}
+
+function clearCalc(){
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    currentDisplayValue.textContent = '';
+    previousDisplayValue.textContent = '';
+}
+
+function delNum(){
+    let num = Array.from(currentInput);
+    num.pop();
+
+    currentInput = num.join('');
+    currentDisplayValue.textContent = currentInput;
+}
+
+numbers.forEach(num => num.addEventListener('click', (e) => {
+    handleNum(e.target.textContent);
 }))
 
-calculate.addEventListener('click', operate());
+operators.forEach(op => op.addEventListener('click', (e) => {
+    handleOp(e.target.textContent);
+}))
 
+equals.addEventListener('click', () => {
+    if(currentInput != '' && previousInput != '') calculate();
+})
+
+clear.addEventListener('click', clearCalc);
+
+backspace.addEventListener('click', delNum);
