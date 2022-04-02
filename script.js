4e740ba51b377c631b90c9007fc1af169d139b07
tProtheroe <tomprothe@hotmail.co.uk>
2022-04-02 15:39:48 +0100

    Complete functionality

--- script.js
+++ script.js
@@ -9,10 +9,8 @@ const previousDisplayValue = document.querySelector('.previous-number');
 let currentInput = '';
 let previousInput = '';
 let operator = ''; 
-let result = 0;
 
 function calculate(){
-
     previousInput = Number(previousInput);
     currentInput = Number(currentInput);
 
@@ -27,33 +25,72 @@ function calculate(){
             previousInput *= currentInput;
             break;
         case '/':
+            if(currentInput == 0){
+                previousInput = "Error";
+                previousDisplayValue.textContent = '';
+                currentDisplayValue.textContent = previousInput;
+                operator = '';
+                return;
+            }
             previousInput /= currentInput;
             break;
     }
 
     roundNum();
-
-    previousDisplayValue.textContent = '';
-    currentDisplayValue.textContent = previousInput
+    previousInput = previousInput.toString();
 }
 
 function handleNum(num){
     if(currentInput.length <= 14){
+        if(num == '.' && currentInput.includes('.')) return;
+        if(currentInput != '' && previousInput != '' && operator == ''){
+            currentInput = '';
+            previousInput = '';
+        }
         currentInput += num;
-        currentDisplayValue.textContent += num;
+        currentDisplayValue.textContent = currentInput;
     }
 }
 
+function displayResult(){
+    if(previousInput.length > 14){
+        currentDisplayValue.textContent = previousInput.slice(0, 12) + '...';
+    } else {
+        currentDisplayValue.textContent = previousInput;
+    }
+    
+    previousDisplayValue.textContent = '';
+
+    operator = '';
+    currentInput = previousInput;
+}
+
 function handleOp(op){
-    operator = op;
-    previousInput = currentInput
-    currentInput = '';
-    previousDisplayValue.textContent = `${previousInput} ${op}`
-    currentDisplayValue.textContent = '';
+    if(currentInput == '') return;
+    
+    if(previousInput == "Can't divide by 0, silly"){
+        currentDisplayValue.textContent = previousInput;
+        previousDisplayValue.textContent = '';
+        clearCalc();
+        
+    } else {
+        if(currentInput != '' && previousInput != ''){
+            calculate();
+            console.log(previousInput);
+        } else {
+            previousInput = currentInput;
+        }
+    
+        currentInput = '';
+        operator = op;
+    
+        currentDisplayValue.textContent = '';
+        previousDisplayValue.textContent = `${previousInput} ${operator}`;
+    }
 }
 
 function roundNum(){
-    previousInput = Math.round(previousInput * 1000) / 1000;
+    if(typeof previousInput == 'number') previousInput = Math.round(previousInput * 100000) / 100000;
 }
 
 function clearCalc(){
@@ -65,10 +102,11 @@ function clearCalc(){
 }
 
 function delNum(){
-    let num = Array.from(currentInput);
-    num.pop();
-
-    currentInput = num.join('');
+    if(currentInput != '' && previousInput != '' && operator == ''){
+        return
+    }
+    
+    currentInput = currentInput.slice(0, -1);
     currentDisplayValue.textContent = currentInput;
 }
 
@@ -81,9 +119,12 @@ operators.forEach(op => op.addEventListener('click', (e) => {
 }))
 
 equals.addEventListener('click', () => {
+    
     if(currentInput != '' && previousInput != '') calculate();
+    displayResult();
 })
 
 clear.addEventListener('click', clearCalc);
 
 backspace.addEventListener('click', delNum);
+
